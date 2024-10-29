e4ab0b93c4fd04999fd6d7435e9c31a4beb63691
Zack <zack@kairaweb.com>
2024-10-29 17:33:07 +0200

    Small JS fixes

--- src/backend/helpers.js
+++ src/backend/helpers.js
@@ -1,4 +1,4 @@
-import { __ } from "@wordpress/i18n";
+import { __ } from '@wordpress/i18n';
 
 /*
  * Convert Text to slug
@@ -6,21 +6,23 @@ import { __ } from "@wordpress/i18n";
 export const blockonsConvertToSlug = (text) => {
 	return text
 		.toLowerCase()
-		.replace(/[^\w ]+/g, "")
-		.replace(/ +/g, "_");
+		.replace(/[^\w ]+/g, '')
+		.replace(/ +/g, '_');
 };
 
 export function blockonsStringReplaceForLink(inputString) {
-	return inputString.replace(
-		/\((.*?)\[\*?(https?:\/\/[^\]]+)\]\)/g,
-		(match, linkText, url) => {
-			// Check if the original text contains the star indicating a new tab target
-			const hasStar = match.includes("[*");
-			// Construct the anchor tag with or without the target="_blank" attribute
-			return `<a href="${url}"${
-				hasStar ? ' target="_blank"' : ""
-			}>${linkText}</a>`;
-		}
+	return (
+		inputString?.replace(
+			/\((.*?)\[\*?(https?:\/\/[^\]]+)\]\)/g,
+			(match, linkText, url) => {
+				// Check if the original text contains the star indicating a new tab target
+				const hasStar = match.includes('[*');
+				// Construct the anchor tag with or without the target="_blank" attribute
+				return `<a href="${url}"${
+					hasStar ? ' target="_blank"' : ''
+				}>${linkText}</a>`;
+			},
+		) ?? ''
 	);
 }
 
@@ -30,154 +32,175 @@ export function blockonsStringReplaceForLink(inputString) {
 export const blockListSettings = {
 	accordions: {
 		desc: __(
-			"Display content in smaller areas with collapsible lists",
-			"blockons"
+			'Display content in smaller areas with collapsible lists',
+			'blockons',
 		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	count_down_timer: {
 		desc: __(
-			"Add a count down block for specials, sales, events, etc",
-			"blockons"
+			'Add a count down block for specials, sales, events, etc',
+			'blockons',
 		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	content_selector: {
 		desc: __(
-			"Let your viewers select which content they want to read.",
-			"blockons"
+			'Let your viewers select which content they want to read.',
+			'blockons',
 		),
 		pluginSpecific: false,
-		isNew: __("NEW", "blockons"),
+		isNew: __('NEW', 'blockons'),
 	},
 	content_toggler: {
 		desc: __(
-			"Show / Hide sections of long content with a toggle button",
-			"blockons"
+			'Show / Hide sections of long content with a toggle button',
+			'blockons',
 		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	icon_list: {
-		desc: __("Visually, more attractive list items with icons", "blockons"),
+		desc: __('Visually, more attractive list items with icons', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	image_comparison: {
-		desc: __("Use a neat action slider to compare 2 images", "blockons"),
+		desc: __('Use a neat action slider to compare 2 images', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	image_gallery: {
-		desc: __("A more advanced gallery with 3 beautiful layouts", "blockons"),
+		desc: __(
+			'A more advanced gallery with 3 beautiful layouts',
+			'blockons',
+		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	image_carousel: {
-		desc: __("Display multiple images in a neat carousel", "blockons"),
+		desc: __('Display multiple images in a neat carousel', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	line_heading: {
-		desc: __("More advanced and customizable line headings", "blockons"),
+		desc: __('More advanced and customizable line headings', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	marketing_button: {
 		desc: __(
-			"An attractive, more trendy customizable call-to-action",
-			"blockons"
+			'An attractive, more trendy customizable call-to-action',
+			'blockons',
 		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	progress_bars: {
-		desc: __("Show progress with beautiful, animated bars", "blockons"),
+		desc: __('Show progress with beautiful, animated bars', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	slider: {
-		desc: __("A slider to display custom content or WC products", "blockons"),
+		desc: __(
+			'A slider to display custom content or WC products',
+			'blockons',
+		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	search: {
-		desc: __("A search bar/icon with drop down or popup search", "blockons"),
+		desc: __(
+			'A search bar/icon with drop down or popup search',
+			'blockons',
+		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	tabs: {
-		desc: __("Display your site information in tabbed content", "blockons"),
+		desc: __('Display your site information in tabbed content', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	table_of_contents: {
-		desc: __("Display a Table of Contents with clickable headings", "blockons"),
+		desc: __(
+			'Display a Table of Contents with clickable headings',
+			'blockons',
+		),
 		pluginSpecific: false,
-		isNew: __("NEW", "blockons"),
+		isNew: __('NEW', 'blockons'),
 	},
 	testimonials: {
-		desc: __("Display client testimonials in a slider or carousel", "blockons"),
+		desc: __(
+			'Display client testimonials in a slider or carousel',
+			'blockons',
+		),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	video_slider: {
-		desc: __("Display multiple videos in a neat video slider", "blockons"),
+		desc: __('Display multiple videos in a neat video slider', 'blockons'),
 		pluginSpecific: false,
 		isNew: false,
 	},
 	wc_account_icon: {
-		desc: __("A simple icon linking to a users WC Account", "blockons"),
-		pluginSpecific: "WooCommerce",
+		desc: __('A simple icon linking to a users WC Account', 'blockons'),
+		pluginSpecific: 'WooCommerce',
 		isNew: false,
 	},
 	wc_featured_product: {
-		desc: __("Display a WC featured product with multple layouts", "blockons"),
-		pluginSpecific: "WooCommerce",
+		desc: __(
+			'Display a WC featured product with multple layouts',
+			'blockons',
+		),
+		pluginSpecific: 'WooCommerce',
 		isNew: false,
 	},
 	wc_mini_cart: {
-		desc: __("A simple WC cart icon with a full cart drop down", "blockons"),
-		pluginSpecific: "WooCommerce",
+		desc: __(
+			'A simple WC cart icon with a full cart drop down',
+			'blockons',
+		),
+		pluginSpecific: 'WooCommerce',
 		isNew: false,
 	},
 };
 
 export const blockonsGroupSettings = () => {
-	const groupBtns = document.querySelectorAll(".blockons-group-btn");
+	const groupBtns = document.querySelectorAll('.blockons-group-btn');
 
 	if (groupBtns) {
 		groupBtns.forEach((btn) => {
-			btn.addEventListener("click", () => {
+			btn.addEventListener('click', () => {
 				const btnParent = btn.parentElement;
 
 				groupBtns.forEach((btnItem) => {
-					btnItem.parentElement.removeAttribute("id", "openGroup");
-					btnItem.parentElement.classList.remove("blockons-show");
+					btnItem.parentElement.removeAttribute('id', 'openGroup');
+					btnItem.parentElement.classList.remove('blockons-show');
 				});
 
 				// Add / Remove .blockons-show class
-				if (btnParent.classList.contains("blockons-show")) {
-					btnParent.removeAttribute("id", "openGroup");
-					btnParent.classList.remove("blockons-show");
+				if (btnParent.classList.contains('blockons-show')) {
+					btnParent.removeAttribute('id', 'openGroup');
+					btnParent.classList.remove('blockons-show');
 				} else {
-					btnParent.setAttribute("id", "openGroup");
-					btnParent.classList.add("blockons-show");
+					btnParent.setAttribute('id', 'openGroup');
+					btnParent.classList.add('blockons-show');
 				}
 			});
 		});
 	}
 
 	// Close on click outside
-	window.addEventListener("click", function (e) {
-		const openGroup = document.getElementById("openGroup");
+	window.addEventListener('click', function (e) {
+		const openGroup = document.getElementById('openGroup');
 
 		if (openGroup) {
 			if (!e.target == openGroup || !openGroup.contains(e.target)) {
-				openGroup.removeAttribute("id");
-				openGroup.classList.remove("blockons-show");
+				openGroup.removeAttribute('id');
+				openGroup.classList.remove('blockons-show');
 			}
 		}
 	});
--- src/frontend/site-addons/siteby/SiteBy.js
+++ src/frontend/site-addons/siteby/SiteBy.js
@@ -1,12 +1,17 @@
 // import React, { useState } from "react";
-import { useState } from "@wordpress/element";
-import { __ } from "@wordpress/i18n";
-import { blockonsStringReplaceForLink } from "../../../backend/helpers";
-import parse from "html-react-parser";
-import "./siteby.css";
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { blockonsStringReplaceForLink } from '../../../backend/helpers';
+import parse from 'html-react-parser';
+import './siteby.css';
 
 const SiteBy = ({ sitebyOptions, isPro, isAdmin }) => {
-	const siteby = sitebyOptions ? sitebyOptions : { enabled: false };
+	const siteby = sitebyOptions
+		? sitebyOptions
+		: {
+				enabled: false,
+				text: 'Site features by (blockons[*https://blockons.com/])',
+			};
 	const isPremium = isPro ? Boolean(isPro) : false;
 	const inAdmin = isAdmin ? Boolean(isAdmin) : false;
 	const [siteByOn, setSiteByOn] = useState(false);
@@ -16,20 +21,24 @@ const SiteBy = ({ sitebyOptions, isPro, isAdmin }) => {
 	return (
 		<div
 			className={`blockons-siteby ${
-				siteby.position !== "bottom" ? siteby.position : "bottom"
-			} ${siteByOn ? "ison" : ""} ${isAdmin ? "inadmin" : ""}`}
-			{...(siteby.position !== "bottom"
+				siteby.position !== 'bottom' ? siteby.position : 'bottom'
+			} ${siteByOn ? 'ison' : ''} ${isAdmin ? 'inadmin' : ''}`}
+			{...(siteby.position !== 'bottom'
 				? {
 						style: {
 							width: `${siteby.size}px`,
 						},
-				  }
+					}
 				: {})}
 		>
-			{siteby.position !== "bottom" && (
+			{siteby.position !== 'bottom' && (
 				<div
 					className={`blockons-siteby-icon fa-solid ${
-						siteby.icon !== "custom" ? siteby.icon : siteby.cicon
+						siteby.icon !== 'custom'
+							? siteby.icon
+							: siteby.cicon
+								? siteby.cicon
+								: 'fa-leaf'
 					}`}
 					onClick={() => setSiteByOn(!siteByOn)}
 					style={{
@@ -44,24 +53,24 @@ const SiteBy = ({ sitebyOptions, isPro, isAdmin }) => {
 
 			<div
 				className={`blockons-siteby-block-wrap ${
-					siteby.position === "bottom" ? "bottom" : "icon"
+					siteby.position === 'bottom' ? 'bottom' : 'icon'
 				}`}
 			>
 				<div
 					className={`blockons-siteby-block ${
-						siteby.position === "bottom" ? "bottom" : "icon"
+						siteby.position === 'bottom' ? 'bottom' : 'icon'
 					}`}
-					{...(siteby.position !== "bottom"
+					{...(siteby.position !== 'bottom'
 						? {
 								style: {
 									height: `${siteby.size}px`,
 									backgroundColor: siteby.bgcolor,
 									color: siteby.color,
 								},
-						  }
+							}
 						: {})}
 				>
-					{parse(blockonsStringReplaceForLink(siteby.text))}
+					{parse(blockonsStringReplaceForLink(siteby.text || ''))}
 				</div>
 			</div>
 		</div>
